0a6d1ff8e95135794b1a15ddc547f425eda418be
bainn gallagher <paujanhapish@gmail.com>
2026-03-01 17:09:03 +0700

    refactor: change from fmt.errorf + errors.new

--- controller/hash/hash.go
+++ controller/hash/hash.go
@@ -1,7 +1,7 @@
 package hash
 
 import (
-	"fmt"
+	"errors"
 
 	"golang.org/x/crypto/bcrypt"
 )
@@ -9,14 +9,14 @@ import (
 func HashPassword(pass string) (string, error) {
 	hashPass, err := bcrypt.GenerateFromPassword([]byte(pass), 9)
 	if err != nil {
-		return "", fmt.Errorf("internal server error: failed hash password: %w", err)
+		return "", errors.New("internal server error: failed hash password: " + err.Error())
 	}
 	return string(hashPass), nil
 }
 
 func UnHashPassword(pass string, hashPass string) error {
 	if err := bcrypt.CompareHashAndPassword([]byte(hashPass), []byte(pass)); err != nil {
-		return fmt.Errorf("invalid password")
+		return errors.New("invalid password")
 	}
 	return nil
 }
